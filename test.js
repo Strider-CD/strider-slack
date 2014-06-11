0cae42e9913fe6b377cbd86add5d0e54870e2c7f
Keyvan Fatehi <keyvanfatehi@gmail.com>
2014-06-11 08:32:22 -0700

    add a few more tests

--- test.js
+++ test.js
@@ -7,25 +7,64 @@ var expect = require('chai').expect;
 var Slack = require('slack-node');
 
 describe("the hack", function() {
-  var hack = require('./index.js')("foo-project")
+  var projName = 'foo-project';
+  var hack = require('./index.js')(projName);
+  var textIncludes = function(sample) {
+    return RegExp(sample).test(get('text'));
+  };
+  var get = function(attr) {
+    return hack.slack.webhook.getCall(0).args[0][attr];
+  };
+  var sendsWebhook = function(exp) {
+    expect(hack.slack.webhook.callCount).to.eq(1);
+    var text = get('text');
+    expect(/pass/.test(text)).to.eq(!!exp);
+    expect(/fail/.test(text)).to.eq(!exp);
+  }
+
   beforeEach(function() {
     sinon.stub(hack.slack, 'webhook');
   });
+
   afterEach(function() {
     hack.slack.webhook.restore();
   });
 
-  it("says it passed if it tests true", function() {
-    hack.tested(true);
-    var text = hack.slack.webhook.getCall(0).args[0].text;
-    expect(/pass/.test(text)).to.be.true;
-    expect(/fail/.test(text)).to.be.false;
+  describe("the webhook", function() {
+    beforeEach(function() {
+      hack.tested(true);
+    });
+
+    it("includes project name", function() {
+      expect(textIncludes(projName)).to.be.true;
+    });
+
+    it("comes from a user named Strider-CD", function() {
+      expect(get('username')).to.eq('Strider-CD');
+    });
+
+    it("goes to room #general", function() {
+      expect(get('channel')).to.eq('#general');
+    });
   });
 
-  it("says it failed if it tests false", function() {
-    hack.tested(false);
-    var text = hack.slack.webhook.getCall(0).args[0].text;
-    expect(/pass/.test(text)).to.be.false;
-    expect(/fail/.test(text)).to.be.true;
+  describe("on test pass", function() {
+    beforeEach(function() {
+      hack.tested(true);
+    });
+
+    it("sends webhook saying it passed", function() {
+      sendsWebhook(true);
+    });
+  });
+
+  describe("on test fail", function() {
+    beforeEach(function() {
+      hack.tested(false);
+    });
+
+    it("sends webhook saying it failed", function() {
+      sendsWebhook(false);
+    });
   });
 });
