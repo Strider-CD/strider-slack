aec9680dbd24cdab344c4cd895319e259f54e8e4
Keyvan Fatehi <keyvanfatehi@gmail.com>
2014-06-11 18:38:33 -0700

    add optional configs

--- webapp.js
+++ webapp.js
@@ -4,6 +4,17 @@ module.exports = {
   config: {
     token: { type: String, default: '' },
     subdomain: { type: String, default: '' },
+    channel: { type: String, default: '#general' },
+    username: { type: String, default: '<%= project.name %>' },
+    icon_url: { type: String, default: 'http://media.stridercd.com/img/logo.png' },
+    test_pass_text: {
+      type: String,
+      default: ':white_check_mark: Tests are passing!'
+    },
+    test_fail_text: {
+      type: String,
+      default: ':exclamation: Tests are failing!'
+    },
   },
   listen: function (io, context) {
     io.on('plugin.slack.fire', function(token, subdomain, payload) {
