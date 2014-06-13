4ce051d2300c9eed2b8f4af0124c556145728bb7
Keyvan Fatehi <keyvanfatehi@gmail.com>
2014-06-12 19:32:19 -0700

    test commit link

--- test/worker_test.js
+++ test/worker_test.js
@@ -15,18 +15,7 @@ describe("worker", function() {
   var job = null;
   var exitCode = null;
 
-  beforeEach(function(done) {
-    exitCode = 0;
-    job = {
-      project: { name: "strider-slack" },
-      ref: { },
-      _id: "123"
-    };
-    process.env.strider_server_name = "http://example.com"
-    config = {};
-    _.each(schema, function(v,k) { config[k] = v.default });
-    config.token = 'token';
-    config.subdomain = 'subdomain';
+  var prepareWorker = function(done) {
     context = {
       comment: sinon.stub()
     };
@@ -37,25 +26,65 @@ describe("worker", function() {
     };
     Worker.init(config, job, sinon.stub(), function(err, res) {
       expect(context.comment).not.to.have.been.called;
-      work = function() { res.listen(io, context) };
-      done();
+      work = function() {
+        res.listen(io, context);
+        return io.emit.getCall(0).args[3];
+      };
+      if (done) done();
     })
+  };
+
+  beforeEach(function(done) {
+    exitCode = 0;
+    job = {
+      project: { name: "strider-slack" },
+      ref: { branch: "master" },
+      _id: "123",
+      trigger: { 
+        type: "manual"
+      }
+    };
+    process.env.strider_server_name = "http://example.com"
+    config = {};
+    _.each(schema, function(v,k) { config[k] = v.default });
+    config.token = 'token';
+    config.subdomain = 'subdomain';
+    prepareWorker(done);
   });
 
-  describe("ref = { branch: 'master' }", function() {
+  it("emits properly", function() {
+    var out = work();
+    expect(io.emit).to.have.been.calledWith('plugin.slack.fire', 'token', 'subdomain');
+    expect(out.channel).to.eq('#general');
+    expect(out.username).to.eq('strider-slack');
+    expect(out.icon_url).to.eq('http://media.stridercd.com/img/logo.png');
+    expect(out.text.length).to.be.greaterThan(10);
+  });
+
+  describe("test pass text", function() {
     beforeEach(function() {
-      job.ref = { branch: "master" }
       work();
     });
-    
-    it("creates a link to the commit", function() {
-      expect(io.emit).to.have.been.calledWith('plugin.slack.fire', 'token', 'subdomain', {
-        channel: '#general',
-        username: 'strider-slack',
-        icon_url: 'http://media.stridercd.com/img/logo.png',
-        text: ':white_check_mark: Tests are passing for master branch :: <http://example.com/strider-slack/job/123|logs>' 
+
+    describe("manual trigger", function() {
+      it("does not link to a commit but shows branch", function() {
+        expect(work().text).to.eq(':white_check_mark: (master) Tests are passing :: <http://example.com/strider-slack/job/123|logs>')
+      });
+    });
+
+    describe("commit trigger", function() {
+      var url = "https://github.com/xyz";
+      var message = "my commit message";
+      beforeEach(function() {
+        job.ref.id = 'appears only on commit trigger';
+        job.trigger.type = "commit";
+        job.trigger.message = message+"\n\n";
+        job.trigger.url = url;
+        prepareWorker();
+      });
+      it("shows commit message as a link on a new line", function() {
+        expect(work().text).to.eq(":white_check_mark: (master) Tests are passing :: <http://example.com/strider-slack/job/123|logs>\n<"+url+"|"+message+">")
       });
     });
   });
-
 });
