ac70f5a2b085e155017a41d8aaca8efa86181cfa
Keyvan Fatehi <keyvanfatehi@gmail.com>
2014-10-23 12:30:46 -0700

    start making the slack plugin suck less

--- webapp.js
+++ webapp.js
@@ -1,5 +1,6 @@
 var Slack = require('slack-node');
 var fs = require('fs');
+var path = require('path')
 
 module.exports = {
   config: require('./schema'),
@@ -7,15 +8,22 @@ module.exports = {
     io.on('plugin.slack.fire', function(token, subdomain, payload) {
       slack = new Slack(token, subdomain);
       slack.webhook(payload, function(err, response) {
-        if (err) console.error(err.message); else console.log(response);
+        if (err)
+          console.error(err.stack);
+        else
+          console.log('Slack response:', response);
       })
     })
   },
   globalRoutes: function (app) {
+    app.get('/bot_avatar', function (req, res) {
+      var filePath = path.join(__dirname, 'static', 'bot_avatar.png')
+      fs.createReadStream(filePath).pipe(res);
+    })
     app.get('/ejs_hint/:kind', function (req, res) {
       if (req.user && req.user.account_level > 0)  {
-        console.log("OK");
-        var filePath = __dirname+'/hints/'+req.params.kind;
+        https://3c2ad4aa.ngrok.com/ext/slack/ejs_hint/manual_retest
+        var filePath = path.join(__dirname, 'hints', req.params.kind)
         if (fs.existsSync(filePath)) {
           var readStream = fs.createReadStream(filePath);
           readStream.pipe(res);
