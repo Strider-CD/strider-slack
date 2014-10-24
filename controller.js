6a096aae307835f46c120a7dcf4a73f5a9b935da
Keyvan Fatehi <keyvanfatehi@gmail.com>
2014-10-23 22:31:03 -0700

    better normalizer

--- controller.js
+++ controller.js
@@ -4,11 +4,13 @@ var _ = { each: require('lodash.foreach') };
 app.controller('SlackController', ['$scope', '$http', function ($scope, $http) {
   var normalizeConfig = function () {
     $scope.config = $scope.configs[$scope.branch.name].slack.config || {};
-    _.each(schema, function(val,key) {
-      if (! $scope.config[key]) $scope.config[key] = val.default;
+    _.each(schema, function(schemaValue,key) {
+      var value = $scope.config[key]
+      if (! value || value.length === 0 )
+        $scope.config[key] = schemaValue.default;
     });
   };
-  normalizeConfig();
+  $scope.normalize = normalizeConfig;
   $scope.$watch('configs[branch.name].slack.config', function (value) {
     $scope.config = value;
   });
