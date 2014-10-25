e8483f98dd01a68c6c5c43ace29a20f85af91227
Keyvan Fatehi <keyvanfatehi@gmail.com>
2014-10-25 12:02:32 -0700

    implement test route

--- webapp.js
+++ webapp.js
@@ -1,28 +1,28 @@
-var Slack = require('slack-node');
-var fs = require('fs');
-var path = require('path')
+var Slack = require('slackihook')
+  , fs = require('fs')
+  , path = require('path')
 
 module.exports = {
   config: require('./schema'),
-  listen: function (io, context) {
-    io.on('plugin.slack.fire', function(token, subdomain, payload) {
-      slack = new Slack(token, subdomain);
-      slack.webhook(payload, function(err, response) {
-        if (err)
-          console.error(err.stack);
-        else
-          console.log('Slack response:', response);
+  globalRoutes: function (app) {
+    app.post('/test', function (req, res) {
+      slack = new Slack(req.body.config.webhookURL);
+      slack.send({
+        channel: req.body.config.channel,
+        username: 'Strider',
+        icon_url: req.body.config.icon_url,
+        text: 'Slack plugin test!'
+      }, function(err, out) {
+        if (err) return res.status(500).end(err.stack);
+        else return res.status(201).end(out);
       })
     })
-  },
-  globalRoutes: function (app) {
     app.get('/bot_avatar', function (req, res) {
       var filePath = path.join(__dirname, 'static', 'bot_avatar.png')
       fs.createReadStream(filePath).pipe(res);
     })
     app.get('/ejs_hint/:kind', function (req, res) {
       if (req.user && req.user.account_level > 0)  {
-        https://3c2ad4aa.ngrok.com/ext/slack/ejs_hint/manual_retest
         var filePath = path.join(__dirname, 'hints', req.params.kind)
         if (fs.existsSync(filePath)) {
           var readStream = fs.createReadStream(filePath);
