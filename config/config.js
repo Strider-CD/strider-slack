c4dd4e68bd195e1142bb66053fb5f6c82a52c1f5
Keyvan Fatehi <keyvanfatehi@gmail.com>
2014-10-25 12:01:35 -0700

    improve HTML and JS

--- config/config.js
+++ config/config.js
@@ -3,21 +3,28 @@ var schema = require('./schema');
 var _ = { each: require('lodash.foreach') };
 
 app.controller('SlackController', ['$scope', '$http', function ($scope, $http) {
-  var normalizeConfig = function () {
+  $scope.fillEmptyFields = function() {
     $scope.config = $scope.configs[$scope.branch.name].slack.config || {};
     _.each(schema, function(schemaValue,key) {
       var value = $scope.config[key]
       if (! value || value.length === 0 )
         $scope.config[key] = schemaValue.default;
     });
-  };
-  $scope.normalize = normalizeConfig;
+  }
+  $scope.normalizeIconURL = function() {
+    var iconURL = $scope.config.icon_url
+    if (iconURL && iconURL[0] === '/') {
+      var root = window.location.protocol+'//'+window.location.host;
+      $scope.config.icon_url = root+$scope.config.icon_url
+    }
+  }
   $scope.$watch('configs[branch.name].slack.config', function (value) {
     $scope.config = value;
   });
   $scope.saving = false;
   $scope.save = function () {
-    normalizeConfig();
+    $scope.fillEmptyFields();
+    $scope.normalizeIconURL()
     $scope.saving = true;
     $scope.pluginConfig('slack', $scope.config, function() {
       $scope.saving = false;
@@ -35,6 +42,25 @@ app.controller('SlackController', ['$scope', '$http', function ($scope, $http) {
       });
     });
   };
+  $scope.test = function() {
+    webhookURL = $scope.config.webhookURL
+    if (webhookURL && webhookURL.length > 0) {
+      $scope.fillEmptyFields()
+      $scope.normalizeIconURL()
+      $scope.testing = true;
+      $http.post('/ext/slack/test', {
+        config: $scope.config
+      }).success(function(data, status, headers, config) {
+        alert('Looks good from here. Check your Slack!')
+      }).error(function(data, status, headers, config) {
+        alert(data)
+      })['finally'](function() {
+        $scope.testing = false;
+      })
+    } else {
+      alert('Webhook URL is required')
+    }
+  }
 }]);
 
 },{"./schema":32,"lodash.foreach":2}],2:[function(require,module,exports){
