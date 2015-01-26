4728f122f52adae6876a75bdb640363110bd0c15
Ilya Radchenko <ilya@burstcreations.com>
2015-01-26 11:47:41 -0500

    Cleanup webapp

--- webapp.js
+++ webapp.js
@@ -1,38 +1,51 @@
-var Slack = require('slackihook')
-  , fs = require('fs')
-  , path = require('path')
+'use stric';
+
+var fs = require('fs');
+var path = require('path');
+var Slack = require('slackihook');
 
 module.exports = {
   config: require('./schema'),
+  
   globalRoutes: function (app) {
     app.post('/test', function (req, res) {
-      slack = new Slack(req.body.config.webhookURL);
+      var slack = new Slack(req.body.config.webhookURL);
+      
       slack.send({
         channel: req.body.config.channel,
         username: 'Strider',
         icon_url: req.body.config.icon_url,
         text: 'Slack plugin test!'
       }, function(err, out) {
-        if (err) return res.status(500).end(err.stack);
-        else return res.status(201).end(out);
-      })
-    })
+        if (err) {
+          return res.status(500).send(err.stack);
+        }
+        else {
+          return res.status(201).send(out);
+        }
+      });
+    });
+    
     app.get('/bot_avatar', function (req, res) {
-      var filePath = path.join(__dirname, 'static', 'bot_avatar.png')
+      var filePath = path.join(__dirname, 'static', 'bot_avatar.png');
+      
       fs.createReadStream(filePath).pipe(res);
-    })
+    });
+    
     app.get('/ejs_hint/:kind', function (req, res) {
       if (req.user && req.user.account_level > 0)  {
-        var filePath = path.join(__dirname, 'hints', req.params.kind)
+        var filePath = path.join(__dirname, 'hints', req.params.kind);
+        
         if (fs.existsSync(filePath)) {
           var readStream = fs.createReadStream(filePath);
+          
           readStream.pipe(res);
         } else {
-          res.send(404);
+          res.status(404).end();
         }
       } else {
-        res.send(401);
+        res.status(401).end();
       }
-    })
+    });
   }
-}
+};
